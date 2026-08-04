47ccc6ce6cc2fcf857b96c25468c3a60d52afa3e
Aidid <aidid23070303@gmail.com>
2026-08-04 10:31:55 +0800

    changes for v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import React from 'react'; 
-import logo from './logo.svg'; 
-import './App.css'; 
- 
-function App() { 
-  return ( 
-    <div className="App"> 
-      <header className="App-header"> 
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <h1>Hello from V2</h1> 
-      </header> 
-    </div> 
-  ); 
-} 
- 
-export default App; 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from V2</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
